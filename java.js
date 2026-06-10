79ee9a970148f677b7e260308809c98a79b1dccf
cristian <cristiangd412@gmail.com>
2026-06-10 13:20:09 -0300

    Delete java.js

--- java.js
+++
@@ -1,5 +0,0 @@
-const cajaBienvenida = document.getElementById("mensaje-bienvenida");
-cajaBienvenida.textContent = "¡Bienvenido a mi sitio Web VOCALOID!";
-const cajaFecha = document.getElementById("fecha-header");
-const hoy = new Date();
-cajaFecha.textContent = "Hoy es: " + hoy.toLocaleDateString('es-AR');
